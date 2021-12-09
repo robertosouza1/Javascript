20af82f12db14af12c57132907861fd41a96cde0
robertosouza1 <robertosouza1@gmail.com>
2021-12-08 22:51:24 -0300

    Iniciando aula 2

---
+++ Aula 2 - Variáveis/script.js
@@ -0,0 +1,13 @@
+// Tipos primitivos
+
+// boolean
+var vOUF = false;
+console.log(vOUF);
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'diana';
+console.log(typeof(nome));
