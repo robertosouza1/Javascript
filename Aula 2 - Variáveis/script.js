e2b203004124c12bc61089d6420493926abc8c22
robertosouza1 <robertosouza1@gmail.com>
2021-12-09 23:52:29 -0300

    Iniciando aula 2

--- Aula 2 - Variáveis/script.js
+++ Aula 2 - Variáveis/script.js
@@ -11,3 +11,21 @@ console.log(typeof(numeroQualquer));
 // string
 var nome = 'diana';
 console.log(typeof(nome));
+
+// function
+var funcao = function () {}
+console.log(typeof(funcao));
+
+// como declarar
+var variavel;
+console.log(variavel);
+
+/* var escopoglobal = 'global';
+console.log(escopoglobal); */
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal();
